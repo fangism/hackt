d6bc89a73a59e2556da06b1b57920399f786c875
David Fang <fang@csl.cornell.edu>
2004-06-06 20:44:43 +0000

    query-able maps and hash_maps

---
+++ hash_qmap.h
@@ -0,0 +1,68 @@
+// "hash_qmap.h"
+
+#ifndef	__HASH_QMAP_H__
+#define	__HASH_QMAP_H__
+
+#include "hash_map.h"			// version independent
+// using directive should be in the below namespace...
+
+#ifndef	HASH_QMAP_NAMESPACE
+#define	HASH_QMAP_NAMESPACE		fang
+#endif
+
+namespace HASH_QMAP_NAMESPACE {
+
+/**
+	Extension of Standard Template Library's map container.  
+	Adds an lookup operator with constant semantics for querying
+	whether or not something is in the map, but without modifying it.  
+	Useful for maps of pointers and pointer classes.  
+ */
+template <class K, class T>
+class hash_qmap : public hash_map<K,T> {
+private:  
+	typedef hash_map<K,T>				parent;
+public:
+	typedef typename parent::iterator		iterator;
+	typedef typename parent::const_iterator		const_iterator;
+public:
+	/** Default constructor. */
+	hash_qmap() : parent() { }
+
+	/** Default destructor. */
+	~hash_qmap() { }
+
+	/**
+		Just wrapper to the parent's lookup operation, 
+		which modifies the map even when querying for a
+		non-existent entry.  
+		Explicitly written here so the const version below
+		doesn't overshadow the parent's non-const version.  
+		\param k the key used to lookup.  
+		\return modifiable reference to the object if found, 
+			else a freshly constructed object.  
+	 */
+	T& operator [] (const K& k) { return parent::operator[](k); }
+
+	/**
+		Constant query associative lookup.  
+		Since it's constant, it won't modify the map
+		by making a blank entry at the given key, 
+		which is what map<K,T> does.  
+		\param k the key used to lookup.  
+		\return read-only COPY of the object if found, 
+			else a freshly constructed object.  
+	 */
+	const T operator [] (const K& k) const {
+		const_iterator i = find(k);	// uses find() const;
+		return (i != end()) ? i->second : T();
+		// if T is a pointer class, should be equivalent to NULL
+		// or whatever the default constructor is
+	}
+
+};	// end class hash_qmap
+
+}	// end namespace
+
+#endif	//	__HASH_QMAP_H__
+
