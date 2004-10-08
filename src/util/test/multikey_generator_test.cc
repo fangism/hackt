90e07f54958252fbd77cd914622e737bbeaabb6d
David Fang <fang@csl.cornell.edu>
2004-10-08 20:42:05 +0000

    new test file(s) for data structure extensions

---
+++ src/util/test/multikey_generator_test.cc
@@ -0,0 +1,29 @@
+// "multikey_generator_test.cc"
+
+#include <iostream>
+#include "multikey.h"
+
+using namespace std;
+using namespace MULTIKEY_NAMESPACE;
+
+int
+main (int argc, char* argv[]) {
+	typedef	multikey<3,int>			key_type;
+	typedef	multikey_generator<3,int>	generator_type;
+	key_type min, max;
+	min[0] = 1;
+	min[1] = 4;
+	min[2] = 7;
+	max[0] = 3;
+	max[1] = 6;
+	max[2] = 9;
+	generator_type iter(min, max);
+	iter.initialize();
+	const key_type end(iter);
+	do {
+		cout << iter[0] << ", " << iter[1] << ", " << iter[2] << endl;
+		iter++;
+	} while (iter != end);
+	return 0;
+}
+
