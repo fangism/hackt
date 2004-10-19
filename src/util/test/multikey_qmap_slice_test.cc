7ca135fcd90c415fc26a14530956c87f3f290e27
David Fang <fang@csl.cornell.edu>
2004-10-19 19:56:46 +0000

    testing files for multidimensional slice manipulations

---
+++ src/util/test/multikey_qmap_slice_test.cc
@@ -0,0 +1,26 @@
+/**
+	\file "multikey_qmap_slice_test.cc"
+	Testing the ability to detect dense sub-arrays.
+ */
+
+#include "cube_slice.h"
+using namespace std;
+using namespace testing;
+
+int
+main(int argc, char* argv[]) {
+	// truncate upper corner
+	cube_slice test1(10,20,30, 14,24,34, 60,68);
+	test1.slice_test_1d();
+	test1.slice_test_2d();
+	test1.slice_test_3d();
+
+	// truncate upper and lower corners
+	cube_slice test2(0,10,20, 4,14,24, 32,40);
+	test2.slice_test_1d();
+	test2.slice_test_2d();
+	test2.slice_test_3d();
+
+	return 0;
+}
+
