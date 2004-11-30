5032f01ee14b25174e361d03b3c9f8a4e2e1cd20
David Fang <fang@csl.cornell.edu>
2004-11-30 02:32:50 +0000

    config.h in this directory just wraps around the ../config.h generated by configure.

---
+++ src/config.h
@@ -0,0 +1,23 @@
+/**
+	\file "config.h"
+	Convenient wrapper for the one automatically generated
+	by configure.
+	$Id: config.h,v 1.1 2004/11/30 02:32:50 fang Exp $
+ */
+
+#ifndef	__CONFIG_H__
+#define	__CONFIG_H__
+
+// NOTE: the generated "config.h" doesn't have include-once protection!
+
+// One can predefine -DIGNORE_CONFIG_H to suppress it, 
+// but good luck trying to compile everything without it!
+// (Decent chance of success, since config.h wasn't 
+// depended upon for a long time.)
+
+#ifndef	IGNORE_CONFIG_H
+#include "../config.h"
+#endif
+
+#endif	// __CONFIG_H__
+
