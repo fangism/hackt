7ca34ca9cda52c1fd948aba97dbb35e8a47afd8e
David Fang <fang@csl.cornell.edu>
2004-11-30 01:26:23 +0000

    Never use these anymore... they are obsolete.

---
+++ src/util/obsolete/ptrs_fwd.h
@@ -0,0 +1,27 @@
+/**
+	\file "ptrs_fwd.h"
+	Forward declarations for non-counted pointer classes in "ptrs.h".
+	$Id: ptrs_fwd.h,v 1.1 2004/11/30 01:26:23 fang Exp $
+ */
+
+#ifndef	__PTRS_FWD_H__
+#define	__PTRS_FWD_H__
+
+// overrideable namespace
+#ifndef	PTRS_NAMESPACE
+#define	PTRS_NAMESPACE			ptrs_ns
+#endif
+
+namespace PTRS_NAMESPACE {
+
+template <class>	class some_ptr;		// sometimes owner
+template <class>	class some_const_ptr;	// sometimes owner
+template <class>	class excl_ptr;		// exclusive owner and deleter
+template <class>	class excl_const_ptr;	// exclusive owner and deleter
+template <class>	class never_ptr;	// never owner
+template <class>	class never_const_ptr;	// never owner
+
+}	// end namespace PTRS_NAMESPACE
+
+#endif	//	__PTRS_FWD_H__
+
