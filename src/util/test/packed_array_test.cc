2f51c08a13482d1f314f0631bb54731d3995c7e2
David Fang <fang@csl.cornell.edu>
2004-12-19 07:51:12 +0000

    more functionality added to packed_array and checked

--- src/util/test/packed_array_test.cc
+++ src/util/test/packed_array_test.cc
@@ -1,14 +1,19 @@
 /**
 	\file "packed_array_test.cc"
-	$Id: packed_array_test.cc,v 1.2 2004/12/16 03:50:59 fang Exp $
+	$Id: packed_array_test.cc,v 1.3 2004/12/19 07:51:12 fang Exp $
  */
 
 #include "packed_array.tcc"
 
 using util::packed_array;
+using util::multikey;
+using util::multikey_generator;
+
 #include "using_ostream.h"
+using std::inner_product;
 
 typedef	packed_array<3, float>	float_3d;
+typedef	packed_array<3, bool>	bool_3d;
 
 int
 main(int argc, char* argv[]) {
@@ -34,6 +39,28 @@ main(int argc, char* argv[]) {
 	bar.resize(ind);
 	assert(!bar.range_check(ind));
 	bar.dump(cerr) << endl;
+
+	ind[0]++; ind[1]++;
+	bar.resize(ind);
+	assert(!bar.range_check(ind));
+	bar.dump(cerr) << endl;
+	// should be 3 x 4 x 4 now
+
+{
+	float_3d::key_generator_type key_gen;
+	key_gen.upper_corner = ind -float_3d::ones;
+	key_gen.lower_corner = float_3d::key_type();
+	float_3d::key_type scale;
+	scale[0] = 100; scale[1] = 10; scale[2] = 1;
+	do {
+		const size_t val =
+			inner_product(key_gen.begin(), key_gen.end(), 
+			scale.begin(), 0);
+		bar[key_gen] = val;
+		key_gen++;
+	} while (key_gen != key_gen.lower_corner);
+	bar.dump(cerr) << endl;
+}
 
 	return 0;
 }
