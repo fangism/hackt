e7a90492c9accf8df5111fe695bcceafd9917d2c
David Fang <fang@csl.cornell.edu>
2005-01-19 01:21:32 +0000

    compile-time integer evaluation

---
+++ src/util/numeric/integer_traits.h
@@ -0,0 +1,303 @@
+/**
+	\file "numeric/integer_traits.h"
+	The templates in this file allow compile time decisions
+	based on traits of constant integer values.  
+	$Id: integer_traits.h,v 1.1.2.1 2005/01/19 01:21:32 fang Exp $
+ */
+
+#ifndef	__UTIL_NUMERIC_INTEGER_TRAITS_H__
+#define	__UTIL_NUMERIC_INTEGER_TRAITS_H__
+
+#include <cstddef>
+	// for size_t
+#include "ifthenelse.h"
+
+//=============================================================================
+// tags for natural number traits
+
+namespace util {
+namespace numeric {
+
+struct prime_tag { };		typedef	prime_tag	not_composite_tag;
+struct composite_tag { };	typedef	composite_tag	not_prime_tag;
+
+//=============================================================================
+// concept constraints: N <= M, etc...
+// like assertions, but at compile time
+
+//=============================================================================
+
+template <size_t N>
+struct is_power_of_2 {
+	enum {	value = N && !(N & N-1) };
+};
+
+/**
+	The number of significant bits, or position of most significant bit.
+ */
+template <size_t N>
+struct significance {
+	// static const size_t
+	enum {	value = significance<(N >> 1)>::value +1 };
+};
+
+template <>
+struct significance<0> {
+	enum {	value = size_t(-1) };
+};
+
+template <bool B>
+struct power_of_2_tag { enum { value = B}; };
+
+// power_of_2_tag<true> and power_of_2_tag<false> are different types!
+
+template <size_t N>
+struct power_of_2_traits {
+	typedef	power_of_2_tag<is_power_of_2<N>::value>	category;
+};
+
+// now can use power_of_2_traits<N> to replace multiplications and division
+//	with arithmetic shifts, useful for sizeof() multiplication
+// such operations would be distinguished using power_of_2_tag<true or false>
+
+// multiplies already defined in "operators.h"
+template <size_t N, class I>
+inline
+I
+__multiply_by_constant(const I& val, power_of_2_tag<false> ) {
+	return val * N;
+}
+
+template <size_t N, class I>
+inline
+I
+__multiply_by_constant(const I& val, power_of_2_tag<true> ) {
+	return val << significance<N>::value;
+}
+
+template <size_t N, class I>
+inline
+I
+multiply_by_constant(const I& val) {
+	return __multiply_by_constant<N,I>(val,
+		power_of_2_traits<N>::category());
+}
+
+//=============================================================================
+template <size_t N, size_t L = 1, size_t H = N>
+struct sqrt_floor {
+private:
+	// can cause internal compiler error on instantiation
+	// static const size_t	value = size_t(sqrt(N));
+	enum {
+		mid = (L + H + 1) >> 1		// average, round up
+	};
+
+public:
+	typedef	typename ifthenelse_type<
+				(N < mid*mid), 
+				sqrt_floor<N,L,mid-1>,
+				sqrt_floor<N,mid,H>
+			>::result_type
+		value_type;
+
+	enum {
+		value = value_type::value
+	};
+};
+
+template <size_t N, size_t R>
+struct sqrt_floor<N,R,R> {
+	enum {	value = R	};
+};
+
+template <size_t L, size_t H>
+struct sqrt_floor<0,L,H> {
+	enum {	value = 0	};
+};
+
+template <size_t N>
+struct is_square {
+	// can cause internal compiler error on instantiation
+	enum {
+		value = (N == sqrt_floor<N>::value *sqrt_floor<N>::value)
+	};
+};
+
+//=============================================================================
+template <size_t N>
+struct factorial {
+	enum { value = N * factorial<N-1>::value };
+};
+
+template <>
+struct factorial<0> {
+	enum { value = 1 };
+};
+
+template <>
+struct factorial<1> {
+	enum { value = 1 };
+};
+
+/**
+	Can be smart to trying to prevent overflow.
+	\param N total number.
+	\param M subset, must be <= N.
+ */
+template <size_t N, size_t M>
+struct combinations {
+	/// this is the dumb way of computing
+	enum { value = factorial<N>::value /
+		(factorial<N-M>::value * factorial<M>::value)
+	};
+};
+
+//=============================================================================
+
+template <size_t N>
+struct nth_fibonacci {
+	enum {
+		value = nth_fibonacci<N-1>::value + nth_fibonacci<N-2>::value
+	};
+};
+
+template <>
+struct nth_fibonacci<0> {
+	enum { value = 1 };
+};
+
+template <>
+struct nth_fibonacci<1> {
+	enum { value = 1 };
+};
+
+template <>
+struct nth_fibonacci<2> {
+	enum { value = 2 };
+};
+
+// struct is_fibonacci
+
+//=============================================================================
+// primality: not ready yet
+
+#if 0
+template <size_t>
+struct is_prime;
+
+template <size_t>
+struct prime_after_prime;
+
+#if 0
+template <>
+struct prime_after_prime<0> {	typedef	enum { value = 2 } value_type;	};
+
+template <>
+struct prime_after_prime<1> {	typedef	enum { value = 2 } value_type;	};
+#endif
+
+template <>
+struct prime_after_prime<2> {	typedef	enum { value = 3 } value_type;	};
+
+template <>
+struct prime_after_prime<3> {	typedef	enum { value = 5 } value_type;	};
+
+#if 0
+template <>
+struct prime_after_prime<3> {	typedef	enum { value = 5 } value_type;	};
+#endif
+
+/**
+	Stops when N is prime.
+	\param N is not prime, must be odd.
+ */
+template <size_t N>
+struct next_prime {
+	enum {
+		value = ifthenelse_value<is_prime<N>::value, 
+			size_t, N, next_prime<N+2>::value>::value
+	};
+};
+/**
+	General algorithm for finding next prime at compile time.  
+	If this is prime, the result is itself, to prevent unbounded
+	recursion.
+	\param N is prime.
+ */
+template <size_t N>
+struct prime_after_prime {
+#if 0
+	typedef	enum {
+		// try next odd number
+		try_next = N + ifthenelse_value<is_prime<N>::value, size_t, 
+				0, 
+				ifthenelse_value<(N & 1), size_t, 2, 1>::value
+			>::value
+	} try_next_type;
+#endif
+#if 0
+	typedef	typename ifthenelse_type<is_prime<N+2>::value, 
+				size_t, 
+				typename prime_after_prime<try_next>::value_type
+			>::result_type
+		value_type;
+	enum {
+		value = ifthenelse_value<is_prime<N>::value, value_type, 
+				N, value_type::value>::value
+	};
+#endif
+#if 1
+	enum {
+		value = next_prime<N+2>::value
+	};
+#endif
+};
+
+/**
+	Assumes that all primes < MIN have been tested.
+	\param MIN the smallest number tried.
+ */
+template <size_t N, size_t MIN>
+struct prime_test_loop {
+	enum {
+		value =
+		ifthenelse_value<(N <= MIN * MIN), bool,
+			true, 
+			ifthenelse_value<(N % MIN), bool,
+				prime_test_loop<N,
+					prime_after_prime<MIN>::value
+				>::value, 
+				false
+			>::value
+		>::value
+	};
+};
+
+template <size_t N>
+struct is_prime {
+	enum {
+		value = prime_test_loop<N, 2>::value
+	};
+};
+
+template <>
+struct is_prime<0> {	enum { value = false };	};
+
+template <>
+struct is_prime<1> {	enum { value = false };	};
+
+template <>
+struct is_prime<2> {	enum { value = true };	};
+
+template <>
+struct is_prime<3> {	enum { value = true };	};
+
+#endif	// not ready yet
+
+//=============================================================================
+}	// end namespace numeric
+}	// end namespace util
+
+#endif	// __UTIL_NUMERIC_INTEGER_TRAITS_H__
+
