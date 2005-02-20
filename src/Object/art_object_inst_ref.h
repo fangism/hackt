aa3e012c7ebca56c5d7219eb2890dd7de0e2f388
David Fang <fang@csl.cornell.edu>
2005-02-20 20:55:01 +0000

    branch:	ARTXX-00-01-04-main-00-04-object-01-13-alias-05-instref rev:	ARTXX-00-01-04-main-00-04-object-01-13-alias-05-instref-02
    Cleaned up, ready to merge to parent.

--- src/Object/art_object_inst_ref.h
+++ src/Object/art_object_inst_ref.h
@@ -1,13 +1,11 @@
 /**
 	\file "art_object_inst_ref.h"
 	Class family for instance references in ART.  
-	$Id: art_object_inst_ref.h,v 1.15.16.1.10.2.2.1 2005/02/20 20:35:51 fang Exp $
+	$Id: art_object_inst_ref.h,v 1.15.16.1.10.2.2.2 2005/02/20 20:55:05 fang Exp $
  */
 
 #ifndef	__ART_OBJECT_INST_REF_H__
 #define	__ART_OBJECT_INST_REF_H__
-
-#define	USE_INSTANCE_REFERENCE_TEMPLATE			1
 
 #include "art_object_inst_ref_base.h"
 #include "art_object_instance_base.h"
@@ -20,8 +18,6 @@ using std::istream;
 using namespace util::memory;
 
 //=============================================================================
-#if USE_INSTANCE_REFERENCE_TEMPLATE
-
 #define	INSTANCE_REFERENCE_TEMPLATE_SIGNATURE				\
 template <class Collection, class Parent>
 
@@ -90,7 +86,6 @@ public:
 	VIRTUAL_PERSISTENT_METHODS_DECLARATIONS
 
 };	// end class instance_reference
-#endif	// USE_INSTANCE_REFERENCE_TEMPLATE
 
 //=============================================================================
 // consider relocating to "art_object_inst_ref_data.h"
@@ -130,110 +125,7 @@ protected:
 	using parent_type::collect_transient_info_base;
 	using parent_type::write_object_base;
 	using parent_type::load_object_base;
-public:
 };	// end class datatype_instance_reference
-
-//-----------------------------------------------------------------------------
-#if !USE_INSTANCE_REFERENCE_TEMPLATE
-/**
-	A reference to a simple instance of channel.  
- */
-class channel_instance_reference : public simple_instance_reference {
-private:
-	typedef	channel_instance_reference		this_type;
-	typedef	simple_instance_reference		parent_type;
-public:
-	typedef	channel_instance_collection	instance_collection_type;
-	typedef	channel_alias_connection	alias_connection_type;
-protected:
-//	excl_ptr<index_list>			array_indices;	// inherited
-	const never_ptr<const instance_collection_type>	channel_inst_ref;
-
-protected:
-	channel_instance_reference();
-public:
-	explicit
-	channel_instance_reference(
-		const never_ptr<const channel_instance_collection> ci);
-
-virtual	~channel_instance_reference();
-
-virtual	ostream&
-	what(ostream& o) const;
-
-//	ostream& dump(ostream& o) const;
-
-	never_ptr<const instance_collection_base>
-	get_inst_base(void) const;
-
-protected:
-	excl_ptr<aliases_connection_base>
-	make_aliases_connection_private(void) const;
-
-protected:
-	void
-	collect_transient_info_base(persistent_object_manager& m) const;
-
-	void
-	write_object_base(const persistent_object_manager& m, ostream&) const;
-
-	void
-	load_object_base(const persistent_object_manager& m, istream&);
-
-public:
-	// need to be virtual? for member_instance_reference?
-	VIRTUAL_PERSISTENT_METHODS_DECLARATIONS
-};	// end class channel_instance_reference
-
-//-----------------------------------------------------------------------------
-/**
-	A reference to a simple instance of process.  
- */
-class process_instance_reference : public simple_instance_reference {
-private:
-	typedef	process_instance_reference		this_type;
-	typedef	simple_instance_reference		parent_type;
-public:
-	typedef	process_instance_collection	instance_collection_type;
-	typedef	process_alias_connection	alias_connection_type;
-protected:
-//	excl_ptr<index_list>			array_indices;	// inherited
-	const never_ptr<const instance_collection_type>	process_inst_ref;
-
-protected:
-	process_instance_reference();
-public:
-	explicit
-	process_instance_reference(
-		const never_ptr<const process_instance_collection> pi);
-
-virtual	~process_instance_reference();
-
-virtual	ostream&
-	what(ostream& o) const;
-
-	never_ptr<const instance_collection_base>
-	get_inst_base(void) const;
-
-protected:
-	excl_ptr<aliases_connection_base>
-	make_aliases_connection_private(void) const;
-
-protected:
-	void
-	collect_transient_info_base(persistent_object_manager& m) const;
-
-	void
-	write_object_base(const persistent_object_manager& m, ostream&) const;
-
-	void
-	load_object_base(const persistent_object_manager& m, istream&);
-
-public:
-	// need to be virtual? for member_instance_reference?
-	VIRTUAL_PERSISTENT_METHODS_DECLARATIONS
-};	// end class process_instance_reference
-#endif	// USE_INSTANCE_REFERENCE_TEMPLATE
 
 //=============================================================================
 // classes pint_instance_reference and pbool_instance_reference
