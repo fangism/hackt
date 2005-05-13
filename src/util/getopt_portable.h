718ab9bbb20bb147758189a1d918b6ffabcca4a0
David Fang <fang@csl.cornell.edu>
2005-05-13 21:41:14 +0000

    TODO: configure getopt once we actually use it (not yet)

--- src/util/getopt_portable.h
+++ src/util/getopt_portable.h
@@ -2,7 +2,7 @@
 	\file "util/getopt_portable.h"
 	Configuration-dependent locations for getopt prototypes.  
 	(Not actually used yet.)
-	$Id: getopt_portable.h,v 1.3 2005/05/10 04:51:23 fang Exp $
+	$Id: getopt_portable.h,v 1.4 2005/05/13 21:41:14 fang Exp $
  */
 
 #ifndef __UTIL_GETOPT_PORTABLE_H__
@@ -11,13 +11,15 @@
 /*
 	FreeBSD, Apple (OS X) and Sun systems declare getopt in unistd.h,
 	other systems (Linux) use getopt.h
+	TODO: let configure script figure out where it is.  
 */
 
 #if defined (__APPLE__) || defined(__FreeBSD__) || \
 	(defined (__SVR4) && defined (__sun))
 #include <unistd.h>
 #else
-#include "util/getopt.h"
+#include <unistd.h>
+// #include "util/getopt.h"
 #endif
  
 #endif	// __UTIL_GETOPT_PORTABLE_H__
