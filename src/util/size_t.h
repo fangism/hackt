53872d2adc1cc55d8bf1f6854879501c24a4a165
David Fang <fang@csl.cornell.edu>
2005-08-18 05:41:36 +0000

    slightly better configured header (should've patched several revisions ago)

--- src/util/size_t.h
+++ src/util/size_t.h
@@ -1,7 +1,7 @@
 /**
 	\file "util/size_t.h"
 	Configuration-wrapped header file for the size_t type.
-	$Id: size_t.h,v 1.2.18.1 2005/08/16 03:50:22 fang Exp $
+	$Id: size_t.h,v 1.2.18.2 2005/08/18 05:41:36 fang Exp $
  */
 #ifndef	__UTIL_SIZE_T_H__
 #define	__UTIL_SIZE_T_H__
@@ -11,7 +11,8 @@
 #if defined(HAVE_CSTDDEF) && HAVE_CSTDDEF
 // size_t is usually typedef'd in <stddef.h>
 #include <cstddef>
-// <cstddef> is the standard C++ wrapper for C's <stddef.h>
+#elif defined(HAVE_STDDEF_H) && HAVE_STDDEF_H
+#include <stddef.h>
 #endif
 
 #endif	// __UTIL_SIZE_T_H__
