d506c0e52841c56a6415949f28585cc63f73cb0c
David Fang <fang@csl.cornell.edu>
2005-11-04 23:30:20 +0000

    branch:	ARTXX-00-01-04-main-00-65-parser-03 rev:	ARTXX-00-01-04-main-00-65-parser-03-02
      Added more configuration detection headers for use with
    	main/version.cc.  Now includes CXX, LEX, YACC versions.
      Next: lexer file stack, possibly include search paths...
    	NOTE: may have to update parser test outputs.

--- src/main/version.cc
+++ src/main/version.cc
@@ -3,7 +3,7 @@
 	Converts ART source code to an object file (pre-unrolled).
 	This file was born from "art++2obj.cc" in earlier revision history.
 
-	$Id: version.cc,v 1.2.2.1 2005/11/04 22:23:29 fang Exp $
+	$Id: version.cc,v 1.2.2.2 2005/11/04 23:30:24 fang Exp $
  */
 
 #include <iostream>
@@ -13,6 +13,9 @@
 #include "config.h"
 #include "cvstag.h"
 #include "builddate.h"
+#include "cxx_version.h"
+#include "lexer/lex_version.h"
+#include "parser/yacc_version.h"
 // #include "util/getopt_portable.h"
 
 namespace ART {
@@ -30,7 +33,7 @@ const char
 version::name[] = "version";
 
 const char
-version::brief_str[] = "Shows the HACKT version info.";
+version::brief_str[] = "Shows the HACKT version and configuration info.";
 
 const size_t
 version::program_id = register_hackt_program_class<version>();
@@ -50,11 +53,52 @@ int
 version::main(const int argc, char* argv[], const global_options&) {
 	if (argc > 1) {
 		usage();
+		// don't bother returning
 	}
-	cout << "Version: " << PACKAGE_STRING << endl;
-	cout << "CVS Tag: " << CVSTAG << endl;
-	cout << "Built: " << BUILDDATE << endl;
+	ostream& o(cout);
+	package(o) << endl;
+	cvs(o) << endl;
+	cxx(o) << endl;
+	lex(o) << endl;
+	yacc(o) << endl;
+	build(o) << endl;
 	return 0;
+}
+
+//-----------------------------------------------------------------------------
+ostream&
+version::package(ostream& o) {
+	return o << "Version: " << PACKAGE_STRING;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ostream&
+version::cvs(ostream& o) {
+	return o << "CVS Tag: " << CVSTAG;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ostream&
+version::cxx(ostream& o) {
+	return o << "c++: " << CXX_VERSION;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ostream&
+version::lex(ostream& o) {
+	return o << "lex: " << LEX_VERSION;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ostream&
+version::yacc(ostream& o) {
+	return o << "yacc: " << YACC_VERSION;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ostream&
+version::build(ostream& o) {
+	return o << "Built: " << BUILDDATE;
 }
 
 //-----------------------------------------------------------------------------
@@ -74,7 +118,7 @@ version::parse_command_options(const int argc, char* argv[], options& opt) {
 //-----------------------------------------------------------------------------
 void
 version::usage(void) {
-	cerr << "version: prints HACKT version" << endl;
+	cerr << "version: prints HACKT version and configuration" << endl;
 	cerr << "usage: " << name << endl;
 }
 
