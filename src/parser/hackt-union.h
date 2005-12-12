09176efb12abc20b6c29922a9839c371393c8ff6
David Fang <fang@csl.cornell.edu>
2005-12-12 03:38:26 +0000

    parallel build fix, dependencies, see ChangeLog

--- src/parser/hackt-union.h
+++ src/parser/hackt-union.h
@@ -2,7 +2,7 @@
 	\file "parser/hackt-union.h"
 	Forawrd declarations needed to parse the YYSTYPE union
 	defined in "parser/hackt-parse.h"
-	$Id: hackt-union.h,v 1.2.4.2 2005/12/11 04:41:38 fang Exp $
+	$Id: hackt-union.h,v 1.2.4.3 2005/12/12 03:38:32 fang Exp $
 	This file was formerly known as:
 	Id: art++-union.h,v 1.1 2005/05/22 20:07:22 fang Exp
  */
@@ -11,9 +11,9 @@
 #define	__PARSER_HACKT_UNION_H__
 
 #include "AST/AST_fwd.h"
-#include "AST/prs_fwd.h"
-#include "AST/hse_fwd.h"
-#include "AST/chp_fwd.h"
+#include "AST/PRS_fwd.h"
+#include "AST/HSE_fwd.h"
+#include "AST/CHP_fwd.h"
 
 using namespace HAC::parser;
 
