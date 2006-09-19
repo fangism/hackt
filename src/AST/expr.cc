b6298c1a201abda343db4535b2eb96735fa99690
David Fang <fang@csl.cornell.edu>
2006-09-19 04:11:42 +0000

    branch:	HACKT-00-01-04-main-00 (main trunk) rev:	HACKT-00-01-04-main-00-80-22
    	  Better diagnostic message for: "can't take member of non-scalar."
    	  alternator.hac: definitions for token repeaters, 2-for-1 producers,
    		two different reshufflings, both decent, one more intuitive.
    		Also defined their chains tor 2^K repeaters.
    	  env.hac: 'ev'-style buckets added, wrapped around e1of1 buckets,
    		also includes inactive_reset variants.
    	  Also started mpl/ directory for template metaprogramming constructs.

--- src/AST/expr.cc
+++ src/AST/expr.cc
@@ -1,7 +1,7 @@
 /**
 	\file "AST/expr.cc"
 	Class method definitions for HAC::parser, related to expressions.  
-	$Id: expr.cc,v 1.16 2006/07/30 05:49:12 fang Exp $
+	$Id: expr.cc,v 1.17 2006/09/19 04:11:46 fang Exp $
 	This file used to be the following before it was renamed:
 	Id: art_parser_expr.cc,v 1.27.12.1 2005/12/11 00:45:05 fang Exp
  */
@@ -1283,6 +1283,9 @@ member_expr::check_meta_reference(const context& c) const {
 			inst_ref->dimensions() << "-dimension array, "
 			"must be scalar!  (for now...)  " <<
 			where(*owner) << endl;
+		inst_ref->dump(cerr << "got: ",
+			expr_dump_context::default_value) <<
+				"." << *member << endl;
 		THROW_EXIT;
 	}
 
