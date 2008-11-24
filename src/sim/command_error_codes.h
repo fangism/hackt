0fae4e89ea34e8049c87819d9e14420f8a702ff3
David Fang <fang@csl.cornell.edu>
2008-11-24 20:49:51 +0000

    * HACKT-00-01-04-main-00-82-62-prsim-02:   Branch for adding more user control to error handling policies 	to hacprsim, including interactive mode.   Added more error codes to command_registry.   Next: handle new error codes returned to the interpreter.

---
+++ src/sim/command_error_codes.h
@@ -0,0 +1,42 @@
+/**
+	\file "sim/command_error_codes.h"
+	$Id: command_error_codes.h,v 1.1.2.1 2008/11/24 20:49:51 fang Exp $
+ */
+
+#ifndef	__HAC_SIM_COMMAND_ERROR_CODES_H__
+#define	__HAC_SIM_COMMAND_ERROR_CODES_H__
+
+#include "sim/prsim/devel_switches.h"
+
+namespace HAC {
+namespace SIM {
+
+/**
+	A few reserved exit codes for main functions.  
+	Should use an int for base integer type.
+	TODO: validate the range of these values w.r.t. 
+		return type size or standard enums.
+ */
+struct command_error_codes {
+enum CommandStatus {
+	FATAL = -5,	///< terminate immediately (e.g. assert fail)
+#if PRSIM_NEW_ERROR_POLICIES
+	INTERACT = -4,	///< open a sub-shell to inspect state
+#endif
+	BADFILE = -3,	///< source file not found
+	SYNTAX = -2,	///< bad syntax
+	UNKNOWN = -1,	///< unknown command
+	NORMAL = 0,	///< command executed fine
+	BADARG = 1,	///< other error with input
+	END = 0xFF	///< normal exit, such as EOF
+};	// end enum CommandStatus
+};	// end struct command_error_codes
+
+typedef	command_error_codes::CommandStatus	CommandStatus;
+
+//=============================================================================
+}	// end namespace SIM
+}	// end namespace HAC
+
+#endif	// __HAC_SIM_COMMAND_ERROR_CODES_H__
+
