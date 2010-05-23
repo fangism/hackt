2602c584e691eb08853e77cb76a46e161a1bf8cf
David Fang <fang@csl.cornell.edu>
2010-05-23 17:44:59 +0000

    size_t

--- src/util/array_indexer.h
+++ src/util/array_indexer.h
@@ -1,12 +1,13 @@
 /**
 	\file "util/array_indexer.h"
-	$Id: array_indexer.h,v 1.1 2010/04/19 02:46:11 fang Exp $
+	$Id: array_indexer.h,v 1.2 2010/05/23 17:44:59 fang Exp $
 	Functor for indexing into arrays.
  */
 
 #ifndef	__UTIL_ARRAY_INDEXER_H__
 #define	__UTIL_ARRAY_INDEXER_H__
 
+#include "util/size_t.h"
 #include <functional>
 
 namespace util {
@@ -21,7 +22,7 @@ struct array_indexer_t :
 	typedef	A					array_type;
 	typedef	typename array_type::value_type		value_type;
 	typedef	typename array_type::reference		reference;
-	typedef	typename array_type::const_reference	const_reference;
+//	typedef	typename array_type::const_reference	const_reference;
 
 	array_type&					_array;
 
@@ -29,7 +30,26 @@ struct array_indexer_t :
 	array_indexer_t(array_type& a) : _array(a) { }
 
 	reference
-	operator () (const size_t i) {
+	operator () (const size_t& i) const {
+		return _array[i];
+	}
+};	// end struct array_indexer_t
+
+template <class A>
+struct const_array_indexer_t :
+	public std::unary_function<size_t, typename A::const_reference> {
+	typedef	A					array_type;
+	typedef	typename array_type::value_type		value_type;
+//	typedef	typename array_type::reference		reference;
+	typedef	typename array_type::const_reference	const_reference;
+
+	const array_type&				_array;
+
+	explicit
+	const_array_indexer_t(const array_type& a) : _array(a) { }
+
+	const_reference
+	operator () (const size_t& i) const {
 		return _array[i];
 	}
 };	// end struct array_indexer_t
@@ -41,6 +61,12 @@ template <class A>
 array_indexer_t<A>
 array_indexer(A& a) {
 	return array_indexer_t<A>(a);
+}
+
+template <class A>
+const_array_indexer_t<A>
+array_indexer(const A& a) {
+	return const_array_indexer_t<A>(a);
 }
 
 }	// end namespace util
