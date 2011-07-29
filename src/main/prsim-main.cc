a14fed2701f68b64d7c2ebeefe686eb2d6ff43a2
David Fang <fang@csl.cornell.edu>
2011-07-29 14:34:05 -0700

    hacprsim: splitting most of prsim program class into 	separate module, so that vpi-prsim can potentially 	call prsim::parse_command_options() for 	pasing command-options to (before) $prsim().

---
+++ src/main/prsim-main.cc
@@ -0,0 +1,31 @@
+/**
+	\file "main/prsim-main.cc"
+	Traditional production rule simulator. 
+	$Id: prsim.cc,v 1.31 2011/05/03 19:20:53 fang Exp $
+ */
+
+#include "util/static_trace.h"
+DEFAULT_STATIC_TRACE_BEGIN
+
+#include "main/prsim-main.h"
+#include "main/program_registry.h"
+
+#ifndef	WITH_MAIN
+namespace HAC {
+const size_t
+prsim::program_id = register_hackt_program_class<prsim>();
+}
+#endif
+
+#ifdef	WITH_MAIN
+/**
+	Assumes no global hackt options.  
+ */
+int
+main(const int argc, char* argv[]) {
+	const HAC::global_options g;
+	return HAC::prsim::main(argc, argv, g);
+}
+#endif
+
+DEFAULT_STATIC_TRACE_END
