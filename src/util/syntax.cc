992765e8c6863e3b024ac1ec9cf6c9304a909a25
David Fang <fang@csl.cornell.edu>
2011-11-07 14:14:21 -0800

    lexer/spice-lex.ll: adjust spice card to expect keywords 	after pre-processing by new script   lib/scripts/hacspice-pp.sed: assist in spice tokenization   main/spice-grind.cc: test program to parse spice file 	into syntax tree, just dumps out LISP-structures for now.

--- src/util/syntax.cc
+++ src/util/syntax.cc
@@ -3,14 +3,17 @@
 	$Id: $
  */
 
+#include <iostream>
 #include "util/syntax.h"
 #include "util/memory/count_ptr.tcc"
+#include "util/indent.h"
 
 namespace util {
 namespace syntax {
 // TODO: move lexer/hac_lex.h into util/lexer/
 // line_position should belong to util
 using HAC::lexer::line_position;
+using std::endl;
 
 //=============================================================================
 // class tree_walker method definitions
@@ -60,6 +63,11 @@ string_token::what(ostream& o) const {
 	return o << str;
 }
 
+ostream&
+string_token::dump(ostream& o) const {
+	return o << str;
+}
+
 line_position
 string_token::leftmost(void) const {
 	return pos;
@@ -97,6 +105,19 @@ node_list::push_back(node* n) {
 	push_back(p);
 }
 
+bool
+node_list::contains_list(void) const {
+	const_iterator i(begin()), e(end());
+	for ( ; i!=e; ++i) {
+		if (*i) {
+			const count_ptr<node_list> c(i->is_a<node_list>());
+			if (c) return true;
+			// else continue
+		}
+	}
+	return false;
+}
+
 void
 node_list::accept(tree_walker& v) const {
 	v.visit(*this);
@@ -105,6 +126,37 @@ node_list::accept(tree_walker& v) const {
 ostream&
 node_list::what(ostream& o) const {
 	return o << "(node-list)";	// size?
+}
+
+/**
+	Do a reasonable job of printing nested LISP-structure
+	Consider passing in indent level?
+ */
+ostream&
+node_list::dump(ostream& o) const {
+	const bool nest = contains_list() && (size() > 1);
+	const_iterator i(begin()), e(end());
+	if (nest) {
+		o << "( ";
+		{
+		// newline separated after first item
+			INDENT_SECTION(o);
+			NEVER_NULL(*i);
+			(*i)->dump(o);
+			for (++i ; i!=e; ++i) {
+				(*i)->dump(o << endl << auto_indent);
+			}
+		}
+		o << " )";
+	} else {
+		// just on one line, space separated
+		o << "(";
+		for ( ; i!=e; ++i) {
+			(*i)->dump(o << ' ');
+		}
+		o << " )";
+	}
+	return o;
 }
 
 line_position
