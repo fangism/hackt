f6b299ea8afae85f2366e620358741136a4a1af2
David Fang <fangism@gmail.com>
2025-01-03 17:32:30 -0800

    [c++] Mark typedef unused
    Mark the internal typedef of STATIC_ASSERT
    as unused to appease clang diagnostics.

--- src/util/static_assert.hh
+++ src/util/static_assert.hh
@@ -3,6 +3,7 @@
 	Compile-time assertion, implemented with template struct.
 	Is like a concept/constraint check.  
 	Ah, the wonders of template meta-programming.  
+	TODO: migrate these to C++-native static_assert.
 	$Id: static_assert.hh,v 1.5 2007/08/29 18:56:44 fang Exp $
  */
 
@@ -36,15 +37,17 @@ struct must_be_true<true> {
 	If argument 'x' contains a comma, 
 	wrap x in an additional set of parenthesis to avoid preprocessor
 	argument splitting.
+	Mark type as unused because it is not used to declare data,
+	and to appease compiler unused-diagnostics.
  */
 #define	UTIL_STATIC_ASSERT(x)						\
-	typedef	util::must_be_true<x >::type UNIQUIFY(__check_type_)
+	typedef	util::must_be_true<x >::type UNIQUIFY(__check_type_) __ATTRIBUTE_UNUSED__
 
 /**
 	Template-dependent version.  
  */
 #define	UTIL_STATIC_ASSERT_DEPENDENT(x)					\
-	typedef	typename util::must_be_true<x >::type UNIQUIFY(__check_type_)
+	typedef	typename util::must_be_true<x >::type UNIQUIFY(__check_type_) __ATTRIBUTE_UNUSED__
 
 #endif	// __UTIL_STATIC_ASSERT_H__
 
